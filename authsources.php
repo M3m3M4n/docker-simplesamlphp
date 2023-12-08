9c09c911a967e966a05ccc3d3d18e7fa1d707c61
hoang <nh231296@gmail.com>
2023-12-09 04:40:28 +0700

    files

---
+++ authsources.php
@@ -0,0 +1,45 @@
+<?php
+// These attributes mimic those of Azure AD.
+$test_user_base = array(
+    'http://schemas.microsoft.com/identity/claims/tenantid' => 'ab4f07dc-b661-48a3-a173-d0103d6981b2',
+    'http://schemas.microsoft.com/identity/claims/objectidentifier' => '',
+    'http://schemas.microsoft.com/identity/claims/displayname' => '',
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/groups' => array(),
+    'http://schemas.microsoft.com/identity/claims/identityprovider' => 'https://sts.windows.net/da2a1472-abd3-47c9-95a4-4a0068312122/',
+    'http://schemas.microsoft.com/claims/authnmethodsreferences' => array('http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/password', 'http://schemas.microsoft.com/claims/multipleauthn'),
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => '',
+    'LastName' => '',
+    'FirstName' => ''
+);
+
+$config = array(
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'user1:password' => array_merge($test_user_base, array(
+            'http://schemas.microsoft.com/identity/claims/objectidentifier' => 'f2d75402-e1ae-40fe-8cc9-98ca1ab9cd5e',
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'F1 L1',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user1@example.com',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'F1',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'F1',
+	    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => 'user1@example.com',
+	    'LastName' => 'L1',
+            'FirstName' => 'F1'
+        )),
+        'user2:password' => array_merge($test_user_base, array(
+            'http://schemas.microsoft.com/identity/claims/objectidentifier' => 'f2a94916-2fcb-4b68-9eb1-5436309006a3',
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'F2 L2',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user2@example.com',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'F2',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'F2',
+	    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => 'user2@example.com',
+	    'LastName' => 'L2',
+            'FirstName' => 'F2'
+        )),
+    ),
+);
